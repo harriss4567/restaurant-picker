f775b14662c1d0f3ae1d48ddee3c8cc21d6160cf
Harris <harris@Harriss-MacBook-Air.local>
2022-10-16 23:22:15 -0400

    Add user suggestions for search and URL info

--- restaurant.py
+++ restaurant.py
@@ -2,5 +2,25 @@
 import requests
 import random
 
+# Declare API KEY and header information
+api_key = ''
+headers = {'Authorization': 'Bearer %s' % api_key}
 
+# Get URL for search
+url = 'https://api.yelp.com/v3/businesses/search'
+
+# Obtain User Location and User Cuisine Choice
+user_location = input('What city are you in? ')
+user_cuisine = input('What type of cuisine are you looking for? ')
+
+# Set parameters for search
+params = {
+    'location': user_location,
+    'radius': 5000,
+    'sort_by': 'rating',
+    'open_now': True,
+    'limit': 10
+}
+
+information = requests.get(url, params=params, headers=headers).text
 
